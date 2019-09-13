99ef57ebbb665656854dd7bc8c392e551ef1a8c2
DTomusk <david.tomusk@gmail.com>
2019-09-13 11:25:33 +0100

    Fahrenheit to Celsius conversion

---
+++ f_to_c/target/debug/deps/f_to_c-47100716d306a505.d
@@ -0,0 +1,5 @@
+E:\rust_exercises\Rust-Exercises\f_to_c\target\debug\deps\f_to_c-47100716d306a505.rmeta: src\main.rs
+
+E:\rust_exercises\Rust-Exercises\f_to_c\target\debug\deps\f_to_c-47100716d306a505.d: src\main.rs
+
+src\main.rs:
---
+++ f_to_c/target/debug/f_to_c.d
@@ -0,0 +1,1 @@
+E:\rust_exercises\Rust-Exercises\f_to_c\target\debug\f_to_c.pdb: E:\rust_exercises\Rust-Exercises\f_to_c\src\main.rs
