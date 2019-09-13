89fffce18616f1f4c255392c33a8ccaeee9ec7a2
DTomusk <david.tomusk@gmail.com>
2019-09-13 11:44:02 +0100

    Only want src files and toml

--- f_to_c/target/debug/deps/f_to_c-59445717b7c0b12c.d
+++
@@ -1,5 +0,0 @@
-E:\rust_exercises\Rust-Exercises\f_to_c\target\debug\deps\f_to_c-59445717b7c0b12c.exe: src\main.rs
-
-E:\rust_exercises\Rust-Exercises\f_to_c\target\debug\deps\f_to_c-59445717b7c0b12c.d: src\main.rs
-
-src\main.rs:
--- f_to_c/target/debug/f_to_c.d
+++
@@ -1,1 +0,0 @@
-E:\rust_exercises\Rust-Exercises\f_to_c\target\debug\f_to_c.pdb: E:\rust_exercises\Rust-Exercises\f_to_c\src\main.rs
