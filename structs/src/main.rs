d07efbea4973528fd0b7589854b6e3023275581e
DTomusk <david.tomusk@gmail.com>
2020-04-27 13:50:07 +0100

    Impl, associated functions and such

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,16 +1,49 @@
 // a struct is like a class's attributes
+#[derive(Debug)]
 struct Person {
     // fields are strings not &str because we want instances to own their data
     surname: String,
     height: f64,
+    weight: f64,
     zodiac: String,
     sexy: bool,
 }
 
-// struct without named fields (tuyple struct)
-struct Dimensions(i32, i32, i32);
+impl Person {
+    fn bmi(&self) -> f64 {
+        self.weight / (self.height*self.height)
+    }
 
-// unit type struct with no fields, useful when you want to implement traits without using values 
+    fn related(&self, other: &Person) -> bool {
+        if self.surname == other.surname {
+            true
+        } else {
+            false
+        }
+    }
+
+    // associated function, doesn't take self as a parameter 
+    fn hot_boy(sur: String, zod: String, hei: f64, wei: f64) -> Person {
+        Person {
+            surname: sur,
+            zodiac: zod,
+            height: hei,
+            weight: wei,
+            sexy: true,
+        }
+    }
+}
+
+// struct without named fields (tuyple struct)
+struct Dimensions(f32, f32, f32);
+
+impl Dimensions {
+    fn volume(&self) -> f32 {
+        self.0*self.1*self.2
+    }
+}
+
+// unit type struct with no fields, useful when you want to implement traits without using values
 struct Sword();
 
 fn main() {
@@ -21,6 +54,7 @@ fn main() {
         zodiac: String::from("Aquarium"),
         sexy: true,
         height: 2.01,
+        weight: 50.6
     };
 
     mordecai.sexy = false;
@@ -34,6 +68,7 @@ fn main() {
         zodiac: mikael.zodiac,
         sexy: false,
         height: -7.6,
+        weight: 12.3,
     };
 
     // Struct update syntax to create a new instance from another instance
@@ -42,7 +77,13 @@ fn main() {
         ..mordecai
     };
 
-    let size = Dimensions(9.6, 1.1, 0.0);
+    let size = Dimensions(9.6, 1.1, 32.0);
+
+    println!("{:?}", size.volume());
+
+    println!("{:#?}", titchmel);
+
+    println!("Titchmel's bmi: {:?}", &titchmel.bmi());
 }
 
 fn constructor(surname: String, zodiac: String, sexy: bool) -> Person {
@@ -51,5 +92,6 @@ fn constructor(surname: String, zodiac: String, sexy: bool) -> Person {
         zodiac,
         sexy,
         height: 0.54,
+        weight: 6.63,
     }
 }
