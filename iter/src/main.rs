d19a7cbcd0e5cbcd84a1e11447fa8879b5bcb67d
DTomusk <david.tomusk@gmail.com>
2020-05-13 10:16:40 +0100

    Fun with custom iterators

--- iter/src/main.rs
+++ iter/src/main.rs
@@ -1,7 +1,51 @@
 // iterators implement the Iterator trait which has one method next which either returns Some(next value) or None once iteration is over
+use rand::prelude::*;
+
+struct Miter {
+    value: u16,
+}
+
+impl Miter {
+    fn new() -> Miter {
+        Miter {
+            value: random(),
+        }
+    }
+}
+
+// iterator keeps going until it generates 7
+impl Iterator for Miter {
+    type Item = u16;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.value != 7 {
+            self.value = random();
+            Some(self.value)
+        } else {
+            None
+        }
+    }
+}
 
 fn main() {
-    println!("Hello, world!");
+    // iterator adaptors are methods defined on the iterator trait that allow you to change iterators into different  kinds of iterators
+    let my_vec: Vec<i32> = vec![1, 2, 7, 3, 8, 0, 0, 8];
+
+    // turns my_vec into an iterator and then doubles each value
+    // again, have to specify that the collection is a vector
+    let your_vec: Vec<_> = my_vec.iter().map(|x| 2 * x).collect();
+
+    println!("{:?}", &your_vec);
+
+    let miter = Miter::new();
+    let mut count = 0;
+
+    for m in miter {
+        println!("{}", m);
+        count += 1;
+    }
+
+    println!("It took {} tries to get to 7", count);
 }
 
 #[cfg(test)]
@@ -25,9 +69,19 @@ mod tests {
     fn summation() {
         let vect = vec![2.7, 6.5, 12.1, 5.5];
         let viter = vect.iter();
-        // sum, like next, is a consuming adaptor, calling it uses up the iterator 
+        // sum, like next, is a consuming adaptor, calling it uses up the iterator
         let total: f64 = viter.sum();
         // floating points are fun
         assert_eq!(total, 26.799999999999997);
+    }
+
+    #[test]
+    fn filtre() {
+        let v = vec![1, 7, 2, 21, 3, -3, 7, 90, 12, 5, 64, 28, 1, -14];
+        // filter uses a closuse to filter the items that satisfy an expression
+        // remember, into_iter takes ownership where iter doesn't
+        let w: Vec<_> = v.into_iter().filter(|x| x <= &20).collect();
+
+        assert_eq!(w, vec![1, 7, 2, 3, -3, 7, 12, 5, 1, -14]);
     }
 }
