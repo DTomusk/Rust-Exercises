c5e19295acd5f26fc6c1a34eb3864e9a58d2b545
DTomusk <david.tomusk@gmail.com>
2020-05-30 07:34:34 +0100

    Graceful shutdown

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -6,7 +6,7 @@ use std::sync::Mutex;
 // ThreadPool contains thread JoinHandles
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>
+    sender: mpsc::Sender<Message>
 }
 
 // use type alias to minimise repetition, Job will store a closure that implements FnOnce
@@ -45,7 +45,32 @@ impl ThreadPool {
     {
         let job = Box::new(f);
 
-        self.sender.send(job).unwrap();
+        self.sender.send(Message::NewJob(job)).unwrap();
+    }
+}
+
+// behaviour when threadpool goes out of scope
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        println!("Sending termiante message to all workers.");
+
+        // workers need to be told to stop what they're doing or they'll just pool forever
+        for _ in &self.workers {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+
+        println!("Shutting down all workers");
+
+        // drop each worker individually by calling join on their thread
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+
+            // take gets ownership of the value in the some variant and leaves a none in its place
+            // if however take() returns a none we don't have to do anything because that thread has already been cleaned up
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
     }
 }
 
@@ -53,22 +78,36 @@ impl ThreadPool {
 // workers act as a go between between the threadpool and the actual JoinHandles
 struct Worker {
     id: usize,
-    thread: thread::JoinHandle<()>,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
         let thread = thread::spawn(move || loop {
             // call lock to get the mutex
             // call recv to get a job from the channel
             // unwrapping is rudimentary error handling (or lack thereof)
-            let job = receiver.lock().unwrap().recv().unwrap();
+            let message = receiver.lock().unwrap().recv().unwrap();
 
-            println!("Worker {} got a job; executing", id);
+            match message {
+                Message::NewJob(job) => {
+                    println!("Worker {} got a job; executing", id);
 
-            job();
+                    job();
+                }
+                Message::Terminate => {
+                    println!("Worker {} was told to terminate", id);
+
+                    break;
+                }
+            }
         });
 
-        Worker { id, thread }
+        Worker { id, thread: Some(thread) }
     }
+}
+
+enum Message {
+    NewJob(Job),
+    Terminate
 }
